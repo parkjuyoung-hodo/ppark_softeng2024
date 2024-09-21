ff8c05e71fc7fe2cc88103b0a72866f0b71f5f32
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-09-21 05:06:34 +0000

    1

---
+++ hw04/hw04_01.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    html_str = """
+
+<!DOCTYPE html>
+ <html lang="kr">
+ <head>
+ <meta charset="UTF-8">
+ <script
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<title>넓이 계산</title>
+ </head>
+ <body>
+ <form id="form_id" action="javascript:post_query()">
+ <h2>평수 계산하기</h2>
+ <label>평 :
+ <input type="text" name="p" value="1">
+ <button type="submit">출력</button>
+ </form>
+ <div id="results"></div>
+ <script>
+ function post_query() {
+ $.ajax({
+ type: "GET",
+ url: "http://127.0.0.1:5000/gugudan/",
+ data: $("#form_id").serialize(),
+ success: update_result,
+ dataType: "html"
+ });
+ }
+ function update_result(data) {
+ $("#results").html(data);
+ }
+ </script>
+ </body>
+ </html>
+
+
+    """
+
+    return html_str
+
+
+
+@app.route('/gugudan/')
+def gugudan_arg_html():
+    p = request.args.get("p", "2")
+    html_str = f"{int(p) * 3.3}㎡</strong><br></n>{(int(p) / 3025):.6f}ha"
+    return html_str
+
+
+app.run(debug=True)
