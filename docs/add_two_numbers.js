f262b5db59518550982b4966acbb550772eac219
juhodo <153529896+parkjuyoung-hodo@users.noreply.github.com>
2024-09-29 01:04:29 +0900

    Delete docs directory

--- docs/add_two_numbers.js
+++
@@ -1,5 +0,0 @@
-function doSomething() {
-    let a = document.getElementById('inputA').value;
-    document.getElementById("valueA").innerHTML = a;
-    document.getElementById("valueC").innerHTML = Number(a) / Number(3.3);
-}
