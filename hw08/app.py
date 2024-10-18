5b759f44d8d6470afe6304f2e9dd72e291c8597f
juhodo <153529896+parkjuyoung-hodo@users.noreply.github.com>
2024-10-18 15:32:44 +0900

    hw08

---
+++ hw08/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_page():
+    return render_template('index.html', title='Home')
+
+@app.route('/to_eunbin_page')
+def to_eunbin_page():
+    return render_template('to_eunbin_page.html', title="to_eunbin_page")
+
+@app.route('/to_juyoung_page')
+def to_juyoung_page():
+    return render_template('to_juyoung_page.html', title="to_juyoung_page")
+
+# @app.route('/layout_page')
+# def layout_page():
+#     return render_template('layout.html',title="layout_page")
+
+
+@app.route('/blog')
+def blog_list():
+
+    df = pd.read_csv("data.csv")
+    post_list = []
+    for i, row in df.iterrows():
+        post_list.append({
+            "title": row["title"],
+            "content": row["content"],
+            "date": row["date"]
+        })
+
+    print(post_list)
+    return render_template('blog.html', title="blog post",posts=post_list)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
