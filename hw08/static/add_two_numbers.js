286656fc952f0be5217a84461993bbfaee6ed274
juhodo <153529896+parkjuyoung-hodo@users.noreply.github.com>
2024-10-18 15:38:43 +0900

    Delete hw08 directory

--- hw08/static/add_two_numbers.js
+++
@@ -1,5 +0,0 @@
-function doSomething() {
-    let a = document.getElementById('inputA').value;
-    document.getElementById("valueA").innerHTML = a;
-    document.getElementById("valueC").innerHTML = (Number(a) / 3.3).toFixed(2);
-}
