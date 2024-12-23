0728358697aae862be07309702d81daae9bf3686
juhodo <153529896+parkjuyoung-hodo@users.noreply.github.com>
2024-12-23 12:23:16 +0000

    수정본

--- fin_softeng2024/blog/models.py
+++ fin_softeng2024/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
 # Create your models here.
 class Post(models.Model):
@@ -12,8 +14,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, null= True, on_delete=models.SET_NULL)
+
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}:: {self.author}'
 
 
     def get_absolute_url(self):
