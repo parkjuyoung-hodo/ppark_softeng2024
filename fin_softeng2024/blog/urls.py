d2baab46ce30596eb8a9f94549e5ff04b3c6803d
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-12-27 14:37:21 +0900

    Delete fin_softeng2024 directory

--- fin_softeng2024/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-
-urlpatterns=[
-   path('', views.PostList.as_view(), name='blog'),
-   path('<int:pk>/',views.PostDetail.as_view()),
-]
